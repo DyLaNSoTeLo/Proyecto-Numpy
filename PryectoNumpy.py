d46153eff98742e20ef19b0e77c12c74ef49fc9e
Dylan Sotelo <dylanalzat@gmail.com>
2023-10-04 21:06:48 -0500

    Parte 1 Proyecto Numpy

---
+++ PryectoNumpy.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+import numpy as np
+
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+edades = np.array(data['age'])
+promedio_edad = np.mean(edades)
+
+print(f"El promedio de edad de las personas participantes en el estudio es {promedio_edad}")
