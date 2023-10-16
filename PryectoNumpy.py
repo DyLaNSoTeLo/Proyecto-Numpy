6b2893a6432e50d67250f50d41a18e063adcadec
Dylan Sotelo <dylanalzat@gmail.com>
2023-10-16 13:49:18 -0500

    Parte 2 Pryecto con Pandas

--- PryectoNumpy.py
+++ PryectoNumpy.py
@@ -1,9 +1,25 @@
 from datasets import load_dataset
 import numpy as np
+import pandas as pd
+
+#Parte 1
 
 dataset = load_dataset("mstz/heart_failure")
 data = dataset["train"]
 edades = np.array(data['age'])
 promedio_edad = np.mean(edades)
 
-print(f"El promedio de edad de las personas participantes en el estudio es {promedio_edad}")
+print(f"El promedio de edad de las personas que participaron en el estudio es: {promedio_edad}")
+
+#Parte 2
+
+df = pd.DataFrame(data)
+
+df_muertos = df[df['is_dead'] == 1]
+df_vivos = df[df['is_dead'] == 0]
+
+promedio_edad_muertos = df_muertos['age'].mean()
+promedio_edad_vivos = df_vivos['age'].mean()
+
+print(f"El promedio de edad de las personas que fallecieron en el estudio es: {promedio_edad_muertos}")
+print(f"El promedio de edad de las personas que sobrevivieron en el estudio es: {promedio_edad_vivos}")
